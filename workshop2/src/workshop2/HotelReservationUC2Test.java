a2e518eec2d2c31be530073e5f8c889ffdcc2dca
“siddiq76” <siddiq.khan0786@gmail.com>
2021-02-26 09:56:38 +0530

    add

--- workshop2/src/workshop2/HotelReservationUC2Test.java
+++ workshop2/src/workshop2/HotelReservationUC2Test.java
@@ -21,20 +21,30 @@ public class HotelReservationUC2Test {
 	}
 
 	@Test
-	public void givenDate_shouldReturn_CheapestHotelNameAndRate() throws Exception {
+	public void givenDate_WeekDayshouldReturn_CheapestHotelNameBridgeWood() throws Exception {
 		hotelReservation.addHotelDetails();
-		ArrayList<String> hotelNameList = hotelReservation.calculateCheapestHotelAndRate("2020-09-11", "2020-09-12");
+		ArrayList<String> hotelNameList = hotelReservation.calculateCheapestHotelAndRate("2020-09-14", "2020-09-16");
 		Object[] hotelName = hotelNameList.toArray();
-		Object[] arrayExpectedOutput = { "LakeWood", "BridgeWood" };
+		Object[] arrayExpectedOutput = { "LakeWood" };
 		Assert.assertArrayEquals(arrayExpectedOutput, hotelName);
 	}
-	
+
 	@Test
-	public void givenDateWeekend_shouldReturn_CheapestHotelNameAndRate() throws Exception {
+	public void givenDateWeekend_shouldReturn_CheapestHotelNameBrideWood() throws Exception {
 		hotelReservation.addHotelDetails();
 		ArrayList<String> hotelNameList = hotelReservation.calculateCheapestHotelAndRate("2020-09-12", "2020-09-13");
 		Object[] hotelName = hotelNameList.toArray();
-		Object[] arrayExpectedOutput = {"BridgeWood"};
+		Object[] arrayExpectedOutput = { "BridgeWood" };
 		Assert.assertArrayEquals(arrayExpectedOutput, hotelName);
 	}
+
+	@Test
+	public void givenDateWeekDayWeekend_shouldReturn_CheapestHotelNameGivesBridgeWood() throws Exception {
+		hotelReservation.addHotelDetails();
+		ArrayList<String> hotelNameList = hotelReservation.calculateCheapestHotelAndRate("2020-09-11", "2020-09-12");
+		Object[] hotelName = hotelNameList.toArray();
+		Object[] arrayExpectedOutput = { "BridgeWood" };
+		Assert.assertArrayEquals(arrayExpectedOutput, hotelName);
+	}
+
 }
